d89bd5d33aff2e6f85f34ae4d9f93768fbe52f11
kyyeo <yeokaiyu@gmail.com>
2021-10-24 15:45:36 +0800

    completed up to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let position;
+    if(num==1 || num==2){
+        return 1;
+    }else if(num < 0) {
+        return 'OOPS';
+    }else if(typeof num === 'string'){
+        position = parseInt(num);
+    }else{
+        position = num;
+    };
     
+    let n=1;
+    let m=1;
+    let result=0;
+    let i=3;
+    let loop = true;
+    while(loop){
+        result=n+m;
+        if(position==i){
+            loop=false;
+        }else{
+            n=m;
+            m=result;
+            i++;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
